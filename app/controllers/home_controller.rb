ac7e58a86e257b1b56c2e06aff36077c8e3f9aee
Lee Farrell <lee.farrell@clavisinsight.com>
2015-03-27 10:00:24 +0000

    moved some files around

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,12 @@ class HomeController < ApplicationController
 
   end
 
+  def game
+    @version = params[:version] ? "-#{params[:version]}" : ''
+
+    render :file => "public/game#{@version}"
+  end
+
   def push_data
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   post 'push_data' => 'home#push_data'
 
-  get 'game' => 'home#game'
+  get 'game/:version' => 'home#game'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
